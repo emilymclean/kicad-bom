b61984e25fa3bcdb529015d5a90f8e49932ef77d
Emily McLean <emilymcleandeveloper@gmail.com>
2025-09-04 15:06:13 +1000

    Exit on error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -l
+set -e
 export HOME="/home/kicad"
+
 kicad-cli sch export python-bom -o bom.xml /github/workspace/$1
 python3 /scripts/bom_csv_grouped_by_value.py bom.xml /github/workspace/$2
